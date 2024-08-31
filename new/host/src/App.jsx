63db67a8fbc7f966e97abdc89333aa06c90a6bdc
Elizaveta Nikolaeva <tiishka2@gmail.com>
2024-08-31 19:21:03 +0300

    Working preparing version for sprint 1 stage 3

---
+++ new/host/src/App.jsx
@@ -0,0 +1,60 @@
+import React, {lazy, Suspense, useEffect, useState} from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const Welcome = lazy(() => import('users/Welcome').catch(() => {
+        return {default: () => <div className='error'>Component Welcome is not available!</div>};
+    })
+);
+
+const UserLogin = lazy(() => import('users/UserLogin').catch(() => {
+        return {default: () => <div className='error'>Component UserLogin is not available!</div>};
+    })
+);
+
+const TaskList = lazy(() => import('tasks/TaskList').catch(() => {
+        return {default: () => <div className='error'>Component TaskList list is not available!</div>};
+    })
+);
+
+const App = () => {
+    const [jwt, setJwt] = useState('');
+
+    const handleJwtChange = event => { // Эта функция получает нотификации о событиях изменения jwt
+        setJwt(event.detail)
+    }
+
+    useEffect(() => {
+        addEventListener("jwt-change", handleJwtChange); // Этот код добавляет подписку на нотификации о событиях изменения localStorage
+        return () => removeEventListener("jwt-change", handleJwtChange) // Этот код удаляет подписку на нотификации о событиях изменения localStorage, когда в ней пропадает необходимость
+    }, []);
+
+    return <div className="container">
+        <header className='App-header'>
+            Лабораторная работа по микрофронтендам
+        </header>
+        <section className='App-content'>
+            {jwt ? (
+                <>
+                    <Suspense>
+                        <Welcome jwt={jwt}/>
+                    </Suspense>
+                    <Suspense>
+                        <TaskList jwt={jwt}/>
+                    </Suspense>
+                </>
+            ) : (
+                <Suspense>
+                    <UserLogin/>
+                </Suspense>
+            )}
+        </section>
+    </div>
+};
+
+const rootElement = document.getElementById("app")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App/>)
